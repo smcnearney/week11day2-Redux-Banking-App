010e8ef95fae9ce80137a27e7dbb9c4a9ad72306
Stephen McNearney <stephen@hownow.media>
2021-02-23 15:04:10 -0500

    Added buttons to increment/decrement by 1 via actions on the reducer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
-import { createStore } from 'redux'
+//import { createStore } from 'redux'
 
-console.log("App started!");
+const { createStore } = Redux; //we imported redux in our HTML file - now we are destructuring createStore from Redux after loading it in a script tag in the html
 
 //Store!
 const defaultState = {
@@ -35,12 +35,28 @@ const account = (state=defaultState, action) => {
 }
 
 //give us store methods (getState, dispatch, subscribe, replaceReducer)
-const store = createStore(account);
+const store = createStore(
+    account, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); //enables chrome devtools extension
 
 store.subscribe(() => {
     console.log('subscribing to state changes');
     const state = store.getState();
     console.log("the state is:", state);
+    const balance = document.querySelector('#balance');
+    balance.innerHTML = state.balance;
 });
 
-store.dispatch(actionIncrement);
+const incrementButton = document.querySelector('#add');
+const decrementButton = document.querySelector('#subtract');
+
+incrementButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    store.dispatch(actionIncrement);
+});
+
+decrementButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    store.dispatch(actionDecrement);
+})
+
